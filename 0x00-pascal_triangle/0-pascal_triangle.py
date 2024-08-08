03794cee9b1bb897198eea5c16ac3475948e14db
islamhasib953 <islamhasib953@example.com>
2024-08-08 21:32:25 +0300

    pascal_triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -5,7 +5,7 @@ def pascal_triangle(n):
         traingle = [[1]]
         for i in range(1, n):
             row = [1]
-            row += (traingle[i - 1][j - 1] + traingle[i - 1][j] for j in range(1, i))
+            row += ([traingle[i - 1][j - 1] + traingle[i - 1][j] for j in range(1, i)])
             row.append(1)
             traingle.append(row)
         return traingle
