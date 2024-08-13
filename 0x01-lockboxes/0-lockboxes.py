0919fb410d8d03af00cade2075994ac8d365c3bc
islamhasib953 <islamhasib953@example.com>
2024-08-13 22:28:58 +0300

    lock boxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,29 +1,34 @@
 #!/usr/bin/python3
 """
-lock boxes
+Lock Boxes Module
+
+This module contains the function `canUnlockAll` which determines
+if all boxes can be unlocked given a list of boxes where each box
+contains keys to other boxes.
+
+The first box (boxes[0]) is unlocked. The function returns True if
+all boxes can be opened, otherwise returns False.
 """
 
 
 def canUnlockAll(boxes):
     """
     Determines if all boxes can be unlocked.
-    Prototype: def canUnlockAll(boxes)
-    boxes is a list of lists
-    A key with the same number as a box opens that box
-    You can assume all keys will be positive integers
-        There can be keys that do not have boxes
-    The first box boxes[0] is unlocked
-    Return True if all boxes can be opened, else return False
-    """
 
-    vistited = set()
+    Args:
+        boxes (list of lists): A list of lists where each list contains
+                               the keys to other boxes.
+
+    Returns:
+        bool: True if all boxes can be unlocked, False otherwise.
+    """
+    visited = set()
     should_visit = [0]
+
     while should_visit:
         i = should_visit.pop()
-        if i not in vistited:
-            vistited.add(i)
-        for j in boxes[i]:
-            if j not in vistited:
-                should_visit.append(j)
+        if i not in visited:
+            visited.add(i)
+            should_visit.extend(key for key in boxes[i] if key not in visited)
 
-    return len(vistited) == len(boxes)
+    return len(visited) == len(boxes)
