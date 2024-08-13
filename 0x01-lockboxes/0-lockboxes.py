c388b473a1ca40a70e1711438bcb131c2731f2af
islamhasib953 <islamhasib953@example.com>
2024-08-13 22:29:33 +0300

    lock boxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,34 +1,39 @@
 #!/usr/bin/python3
-"""
-Lock Boxes Module
 
-This module contains the function `canUnlockAll` which determines
-if all boxes can be unlocked given a list of boxes where each box
-contains keys to other boxes.
+'''
+this problem is You have n number of locked boxes in front of you. Each box is
+numbered sequentially from 0 to n - 1 and
+each box may contain keys to the other boxes.
 
-The first box (boxes[0]) is unlocked. The function returns True if
-all boxes can be opened, otherwise returns False.
-"""
+Write a method that determines if all the boxes can be opened.
+
+    Prototype: def canUnlockAll(boxes)
+    boxes is a list of lists
+    A key with the same number as a box opens that box
+    You can assume all keys will be positive integers
+        There can be keys that do not have boxes
+    The first box boxes[0] is unlocked
+    Return True if all boxes can be opened, else return False
+# def realCanUnlockAll(boxes):
+#     unlocked = [0] * len(boxes)
+#     stack = []
+#     unlocked[0] = 1
+#     stack.append(boxes[0])
+#     while len(stack):
+#         box = stack.pop()
+#         for key in box:
+#             if  key < len(boxes) and not unlocked[key]:
+#                 stack.insert(0, boxes[key])
+#                 unlocked[key] = 1
+#     return True if sum(unlocked) == len(boxes) else False
+
+'''
 
 
 def canUnlockAll(boxes):
-    """
-    Determines if all boxes can be unlocked.
-
-    Args:
-        boxes (list of lists): A list of lists where each list contains
-                               the keys to other boxes.
-
-    Returns:
-        bool: True if all boxes can be unlocked, False otherwise.
-    """
-    visited = set()
-    should_visit = [0]
-
-    while should_visit:
-        i = should_visit.pop()
-        if i not in visited:
-            visited.add(i)
-            should_visit.extend(key for key in boxes[i] if key not in visited)
-
-    return len(visited) == len(boxes)
+    keys = [0]
+    for key in keys:
+        for new_key in boxes[key]:
+            if new_key not in keys and new_key < len(boxes):
+                keys.append(new_key)
+    return len(keys) == len(boxes)
