85a3340153e362e98e84a9d3c5e9b42f947b279a
islamhasib953 <islamhasib953@example.com>
2024-08-29 21:05:24 +0300

    solve

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -2,14 +2,11 @@
 
 import signal
 import sys
-import re
 
 count_status = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
 
 total_file_size = 0
 line_processed = 0
-# pattern = r'^\S+ - \[.*\] "GET /projects/260 HTTP/1\.1" (\d+) (\d+)$'
-# pattern = r'^(\S+) - \[(.*?)\] "GET /projects/260 HTTP/1\.1" (\d+) (\d+)$'
 
 
 def print_status():
@@ -27,37 +24,23 @@ def print_status():
             print(f"{status}: {count_status[status]}")
 
 
-def interrupt_handeler(signum, frame):
-    print_status()
-    sys.exit(0)
-
-
-signal.signal(signal.SIGINT, interrupt_handeler)
-
-
 def process_line(line):
     global line_processed
     global total_file_size
     parsed_line = line.split()
     parsed_line = parsed_line[::-1]
-    # match = re.match(pattern, line)
-    try:
-        if len(parsed_line) > 2:
-            # parts = match.groups()
-            total_file_size += int(parsed_line[0])
-            status_code = int(parsed_line[1])
-            # file_size = int(parts[4])
-            # check_status_code = count_status.get(status_code)
-            # if check_status_code is not None:
-            if status_code in count_status.keys():
-                count_status[status_code] += 1
-            # total_file_size+=file_size
-            line_processed += 1
-        if line_processed % 10 == 0:
-            print_status()
-    finally:
+    if len(parsed_line) > 2:
+        total_file_size += int(parsed_line[0])
+        status_code = int(parsed_line[1])
+        if status_code in count_status.keys():
+            count_status[status_code] += 1
+        line_processed += 1
+    if line_processed % 10 == 0:
         print_status()
 
 
-for line in sys.stdin:
-    process_line(line)
+try:
+    for line in sys.stdin:
+        process_line(line)
+finally:
+    print_status()
