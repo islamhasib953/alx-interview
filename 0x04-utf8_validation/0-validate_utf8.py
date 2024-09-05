5dd39ec3d002d8a87cc16ed950cbf61d36368432
islamhasib953 <islamhasib953@example.com>
2024-09-05 20:43:21 +0300

    validUTF8

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -4,21 +4,20 @@
 
 def validUTF8(data):
     """determines if a given data set represents a valid UTF-8 encoding"""
-    for i in range(len(data)):
-        cnt_ones = 0
-        byte = bin(data[i])[2:]
-        if len(byte) == 8:
-            find_zero = byte.find("0")
-            cnt_ones = len(byte[:find_zero])
-            cnt = 1
-            for j in range(1, cnt_ones):
-                i += 1
-                cnt += 1
-                byte_new_data = bin(data[i])[2:]
-                zero = byte_new_data.find("0")
-                ones = len(byte_new_data[:zero])
-                if ones > 1:
-                    return False
-            if cnt < cnt_ones or cnt > cnt_ones:
+    cnt = 0
+    for byte in data:
+        binary = bin(byte).replace("0b", "").rjust(8, "0")
+        if cnt == 0:
+            if binary.startswith("110"):
+                cnt = 1
+            elif binary.startswith("1110"):
+                cnt = 2
+            elif binary.startswith("11110"):
+                cnt = 3
+            elif binary.startswith("10"):
                 return False
-    return True
+        else:
+            if not binary.startswith("10"):
+                return False
+            cnt -= 1
+        return cnt == 0
